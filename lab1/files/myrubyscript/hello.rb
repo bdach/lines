bec98db8c746128dc4657ca27d8a1ab7b60ffa44
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2017-03-30 17:05:59 +0200

    Second session

--- lab1/files/myrubyscript/hello.rb
+++ lab1/files/myrubyscript/hello.rb
@@ -1,5 +1,15 @@
 #!/usr/bin/ruby
+require 'socket'
 
 puts 'Welcome to LINSW!'
 time = Time.new
 puts 'Current time: ' + time.inspect
+
+socket = UDPSocket.new
+socket.bind('', 48000)
+
+while true do
+	sleep 10
+	time = Time.new
+	socket.send time.inspect, 0, '192.168.143.172', 48000
+end
---
+++ lab1/files/sesja2.md
@@ -0,0 +1,1 @@
+co 10 sek pakiet udp na maszynę laboratoryjną z czasem (port >1024)
