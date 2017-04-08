8a388f4cadafe4545da8feb48b332c8ff5b67f11
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2017-04-08 18:30:34 +0200

    package setup & config backup

---
+++ lab2/files/package/simon/simon.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# simon
+#
+################################################################################
+
+SIMON_VERSION = 1.1
+SIMON_SITE = $(TOPDIR)/../simon
+SIMON_SITE_METHOD = local
+
+define SIMON_BUILD_CMDS
+   $(MAKE) $(TARGET_CONFIGURE_OPTS) CFLAGS="$(CFLAGS) -std=gnu99" simon -C $(@D)
+endef
+define SIMON_INSTALL_TARGET_CMDS 
+   $(INSTALL) -D -m 0755 $(@D)/simon $(TARGET_DIR)/usr/bin 
+endef
+SIMON_LICENSE = MIT
+
+$(eval $(generic-package))
